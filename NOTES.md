376c159c1d49b9f42b4f36a467a6597a4b952d6b
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2017-03-17 10:29:13 -0700

    Basic noninteractive cursor

---
+++ NOTES.md
@@ -0,0 +1,18 @@
+Ideas:
+
+Festival:
+
+ - Puff up each furniture, turn it in to a balloon
+ - get rid of the room, make them all balloons in the wind held by a litle girl
+ - click on them and she lets them go (or they get cut)
+ - release all of them, and it zooms out to show a thought bubble in the chair
+ 
+ 
+Discussion:
+ 
+ - Start the scene, and you see vague clock hands in front of your vision.  You can't quite tell what they are  
+ - When you click on a chair, the camera moves in to it, and you become the piece of furniture
+ - you look around, and see that you were originally a grand father clock
+ - remove the floor and have everything float on a cloud
+ - remove the ceiing and have evereything in the stars
+ - add a Bokeh filter
--- SphericalCursor.js
+++ SphericalCursor.js
@@ -1,40 +1,64 @@
-var SphericalCursor = function() {
+var SphericalCursor = function (camera, scene) {
+  'strict mode';
+  this.camera = camera;
+  this.scene = scene;
+  this.SENSITIVITY = 0.01;              // to adjust how sensitive the mouse control is
+  this.DISTANCE_SCALE_FACTOR = -0.05;  // to scale down the cursor based on its collision distance
+  this.DEFAULT_CURSOR_SCALE = 0.6;     // scale to set the cursor if no raycast hit is found
+  this.MAX_DISTANCE = 100;             // maximum distance to raycast
+  this.SPHERE_RADIUS = 20;           // sphere radius to project cursor onto if no raycast hit
+  this.mouse = new THREE.Vector2();
+  this.raycaster = new THREE.Raycaster();
+  this.clickable_objects = [];
+  this.active = false;
 
-	// This class uses Javascript module pattern.
+  this.mesh = new THREE.Mesh(
+    new THREE.SphereGeometry(0.2),
+    new THREE.MeshPhongMaterial({color: "#FFFFFF"})
+  );
+  this.mesh.name = "cursor";
+  this.mesh.material.depthTest = false;
 
-	// CONSTANTS - defaults, adjust if needed
-	var SENSITIVITY = 500;              // to adjust how sensitive the mouse control is
-	var DISTANCE_SCALE_FACTOR = -0.05;  // to scale down the cursor based on its collision distance
-	var DEFAULT_CURSOR_SCALE = 0.6;     // scale to set the cursor if no raycast hit is found
-	var MAX_DISTANCE = 100;             // maximum distance to raycast
-	var SPHERE_RADIUS = 1000;           // sphere radius to project cursor onto if no raycast hit
+  camera.add(this.mesh);
 
-	window.addEventListener( "mousemove", onMouseMove );
+  window.addEventListener("mousemove", this.onMouseMove.bind(this));
+};
 
-	function update() {
+SphericalCursor.prototype = {
+  onMouseMove: function (event) {
+    if (!this.active) return;
+    this.mouse.x += (event.movementX || event.mozMovementX || event.webkitMovementX || 0) * this.SENSITIVITY;
+    this.mouse.y -= (event.movementY || event.mozMovementY || event.webkitMovementY || 0) * this.SENSITIVITY;
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+  },
 
-		// TODO: Write this function.
+  update: function () {
+    if (!this.active) return;
 
-		// Put the cursor where you can see it, delete this.
-		cursor.position.y = 2.0;
+    this.mesh.position.copy(
+      this.raycaster.ray.direction.normalize()
+    ).multiplyScalar(this.SPHERE_RADIUS);
 
-	}
 
-	function onMouseMove( event ) {
+    // console.log(this.mesh.position.toArray());
+    // var intersects = this.raycaster.intersectObjects(this.clickable_objects);
+    // if (intersects.length > 0) {
+    //   intersects[0].object.material.color.setHex(Math.random() * 0xffffff);
+    //   var particle = new THREE.Sprite(particleMaterial);
+    //   particle.position.copy(intersects[0].point);
+    //   particle.scale.x = particle.scale.y = 16;
+    //   scene.add(particle);
+    // }
 
-		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+  },
 
-		// TODO: Write this function.
+  addObject: function (o) {
+    this.clickable_objects.push(o);
+  },
 
-		// For debugging, can remove it.
-		console.log("movementX=" + movementX + ", movementY=" + movementY );
+  activate: function () {
+    this.active = true;
+  }
 
-	}
-
-	return {
-		update: update
-	};
-
-}();
+};
 
