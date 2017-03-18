8508efcde77b8553f18c6b6e7f4a3237bcc95021
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2017-03-18 16:42:09 -0700

    Animation framework

--- js/FloatingText.js
+++ js/FloatingText.js
@@ -5,29 +5,64 @@
 // when it is "clicked", it will be restored.
 
 
-function FloatingText(camera, scene, segments) {
+function TextCtrl(camera, scene, segments) {
 
-  var material = new THREE.MultiMaterial([
-    new THREE.MeshBasicMaterial({color: 0xffffff, overdraw: 0.5}),
-    new THREE.MeshBasicMaterial({color: 0x000000, overdraw: 0.5})
-  ]);
+  this.camera = camera;
+  this.scene = scene;
+  this.meshes = [];
+  this.animations = [];
 
   var loader = new THREE.FontLoader();
-  var size = 0.08;
-  var height = 0.07;
-  var curveSegments = 2;
-  var segment;
 
   loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+    this.font = font;
+    this.createMeshes();
+  }.bind(this));
 
-    for (var i = 0; i < segments.length; i++) {
-      segment = segments[i];
+}
+
+TextCtrl.prototype = {
+  createMeshes: function () {
+    this.segments = [
+      {
+        string: "My dear, let us hope it is not true; but, if it is true, ",
+        cameraPos: ['center', 1.03, -3]
+      },
+      {
+        string: "let us hope it will not become generally known.",
+        cameraPos: ['center', 0.9, -3]
+      }//,
+      // {
+      //   string: "--Charles Darwin's friend, the wife ",
+      //   cameraPos: [0, 0.77, -3],
+      //   geometryOptions: {size: 0.05, height: 0.03}
+      // },
+      // {
+      //   string: "  of the Bishop of Birmingham, 1880s.",
+      //   cameraPos: [0, 0.67, -3],
+      //   geometryOptions: {size: 0.05, height: 0.03}
+      // }
+    ];
+
+    var material = new THREE.MultiMaterial([
+      new THREE.MeshBasicMaterial({color: 0xffffff, overdraw: 0.5}),
+      new THREE.MeshBasicMaterial({color: 0x000000, overdraw: 0.5})
+    ]);
+
+
+    var size = 0.08;
+    var height = 0.07;
+    var curveSegments = 2;
+    var segment;
+
+    for (var i = 0; i < this.segments.length; i++) {
+      segment = this.segments[i];
       var geometryOptions = {
-        font: font, size: size, height: height, curveSegments: curveSegments
+        font: this.font, size: size, height: height, curveSegments: curveSegments
       };
       Object.assign(geometryOptions, segment.geometryOptions);
 
-      var geometry = new THREE.TextGeometry(segment.string, geometryOptions );
+      var geometry = new THREE.TextGeometry(segment.string, geometryOptions);
 
       geometry.computeBoundingBox();
 
@@ -38,22 +73,64 @@ function FloatingText(camera, scene, segments) {
         xOffset = segment.cameraPos[0];
       }
 
-      var mesh = new THREE.Mesh(geometry, material);
+      segment.mesh = new THREE.Mesh(geometry, material);
 
-      mesh.position.set(
+      segment.mesh.position.set(
         xOffset,
         segment.cameraPos[1],
         segment.cameraPos[2]
       );
-      camera.add(mesh);
-      mesh.lookAt(camera);
+      this.camera.add(segment.mesh);
+      segment.mesh.lookAt(this.camera);
     }
-  });
 
-}
+    this.animate(0, new THREE.Vector3(0, 1, -3)).then(function () {
+      console.log('animation complete');
+    });
 
-FloatingText.prototype = {
-  update: function () {
+  },
+
+  // fixed duration of 1s
+  animate: function (index, endPos) {
+    var o = {
+      startTime: performance.now(),
+      endTime: performance.now() + 2000,
+      target: this.segments[index],
+      startPos: this.segments[index].mesh.position.clone(),
+      endPos: endPos,
+      resolve: null,
+      reject: null
+    };
+
+    var p = new Promise(function (resolve, reject) {
+      o.resolve = resolve;
+      o.reject = reject;
+    });
+
+    this.animations.push(o);
+    return p;
+  },
+
+
+  update: function (timestamp) {
+
+    var animation;
+    for (var i = 0; i < this.animations.length; i++) {
+      animation = this.animations[i];
+
+      var t = (timestamp - animation.startTime) / (animation.endTime - animation.startTime);
+      if (t > 1) {
+        this.animations.splice(i, 1);
+        i--;
+        animation.resolve();
+      } else {
+        animation.target.mesh.position.set(0, 0, 0)
+          .add(animation.endPos).sub(animation.startPos).multiplyScalar(t)
+          .add(animation.startPos);
+      }
+    }
 
   }
+
+
 }
